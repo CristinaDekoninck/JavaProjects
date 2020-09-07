54a25adb0e36c109893455fa1ceb9a0c41f7fd7b
CristinaDekoninck <cristina.dekoninck@accenture.com>
2020-09-07 11:58:28 +0100

    Test

--- src/animals/CreateAnimals.java
+++ src/animals/CreateAnimals.java
@@ -1,2 +1,32 @@
-package animals;public class CreateAnimals {
+package animals;
+
+import java.util.Scanner;
+
+public class CreateAnimals{
+
+    Scanner myScanner = new Scanner(System.in);
+
+    private void getAnimalType(){
+        System.out.println("What animal would you like to create?");
+        String animal = myScanner.nextLine();
+
+        if(animal.equalsIgnoreCase("dog")){
+            createDog();
+        }
+        else if (animal.equalsIgnoreCase( "cat")){
+            createcat();
+        }
+        else{
+            System.out.println(animal + " is not an animal we can currently create, please try again.");
+        }
+
+    }
+
+    private void createcat() {
+
+    }
+
+    private void createDog() {
+
+    }
 }
